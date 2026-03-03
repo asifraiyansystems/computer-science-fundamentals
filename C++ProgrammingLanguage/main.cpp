d41ca2068113dac1441eac469050eaa2cc8929ce
asifraiyan-systems <asifraiyan.systems@gmail.com>
2026-03-03 06:20:13 +0600

    C++ First Program

---
+++ C++ProgrammingLanguage/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "This is a Programmer." <<endl;
+    cout << "My name is Asib." <<endl;
+    cout << "I love cookies." <<endl;
+    cout << "I am 5 years old." <<endl;
+    return 0;
+}
